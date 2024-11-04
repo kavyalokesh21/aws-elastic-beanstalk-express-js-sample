5ace32ff57e63ccfcd95cc79a28e1f0cb6674477
kavyaloks21 <kavyal.22mca@jyotinivas.org>
2024-11-04 16:48:54 +0530

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('bravo u did it, Congratulations Buddy'));
+app.get('/', (req, res) => res.send('bravo u did it!, Congratulations Buddy'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
